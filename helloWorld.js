c3dae5b08b3278fa2c153b3243218c76171403a5
AsilbekNosirov <asilbeknosirov20202003@gmail.com>
2023-12-11 22:48:40 +0900

    JS001 - var, arithme exp, input, type cover,const.

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,120 @@
-console.log("Hello world")
+                               //Variables
+
+//console.log("Hello world")
+//
+//let age = 21; //numbers
+//age = age + 1;
+//
+//let firstName = "Bro"; //strings
+//
+//let student = true; //booleans
+//
+//console.log("Hello", firstName);
+//console.log("You are", age, "years old");
+//console.log("Enrolled:", student);
+//
+//document.getElementById("p1").innerHTML = "Hello " + firstName;
+//document.getElementById("p2").innerHTML = "You are " + age + " years old";
+//document.getElementById("p3").innerHTML = "Enrolled: " + student;
+
+
+
+                            //Arithmetic Expressions
+
+// let students = 20;
+
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//let extraStudents = students % 2;
+
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+
+  /*
+      operator precedence
+      1. parenthesis ()
+      2. exponents
+      3. multiplication & division
+      4. addition & subtruction
+  */
+ 
+// let result = (1 + 2) * (3 + 4);
+
+// console.log(result);
+
+
+
+                            // User Input
+
+// How to eccept user input
+
+// EASY WAY with a window prompt
+
+// let username = window.prompt("What's your name?");
+// console.log(username);
+
+// DIFFICULT WAY HTML textbox
+
+// let username;
+
+// document.getElementById("myButton").onclick = function () {
+    
+//     username = document.getElementById("myText").value;
+//     console.log(username);
+//     document.getElementById("myLabel").innerHTML ="Hello" +  username;
+// }
+
+
+                            // Type Conversion 
+
+            // Type Conversion = change the datatype of a value to another
+                        // (strings, numbers, booleans)
+
+
+// let age = window.prompt("How old are you?");
+
+// age = Number(age);
+// age += 1;
+
+// console.log("Happy Birthday! You are", age, "years old");
+
+// let x;
+// let y;
+// let z;
+
+// x = Number("3.14");
+// y = String(3.14);
+// z = Boolean("Osh");
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+
+
+                            //  Const  = a variable that can't be changed
+
+// const PI = 3.14159;
+// let radius;
+// let circumference;
+
+// radius = window.prompt("Enter the radius of a circle");
+// radius = Number(radius);
+
+// // 9PI = 420.69;
+
+// circumference = 2 * PI * radius;
+
+// console.log("The circumference is:", circumference);
+
+
+
+                            //  Math
+
+
+
+
