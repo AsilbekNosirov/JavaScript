bcfe59ff8e42c6ee92a48f0cd32b6394b2b08035
AsilbekNosirov <asilbeknosirov20202003@gmail.com>
2023-12-13 05:01:24 +0900

    JS002-Math,counter prog,random num,string methods.

--- helloWorld.js
+++ helloWorld.js
@@ -48,6 +48,8 @@
 
 
 
+
+
                             // User Input
 
 // How to eccept user input
@@ -69,7 +71,12 @@
 // }
 
 
+
+
+
                             // Type Conversion 
+
+                            
 
             // Type Conversion = change the datatype of a value to another
                         // (strings, numbers, booleans)
@@ -114,6 +121,139 @@
 
 
                             //  Math
+
+
+// let x = 3.14;
+// let y = 5;
+// let z = 9;
+// let maximum;
+// let minimum;
+
+// // x = Math.round(x);
+// // x = Math.floor(x);
+// // x = Math.ceil(x);
+// // x = Math.pow(x, 4);
+// // x = Math.sqrt(x);
+// // x = Math.abs(x);
+// // maximum = Math.max(x, y, z);
+// // minimum = Math.min(x, y, z);
+// x = Math.PI;
+
+// console.log(x);
+
+
+                          // Hypoteuse calc practice program
+
+
+// let a;
+// let b;
+// let c;
+
+/* a = window.prompt("Enter side A");
+a = Number(a);
+
+b = window.prompt("Enter side B");
+b = Number(b);
+
+// c = Math.pow(a, 2) + Math.pow(b, 2);
+// c = Math.sqrt(c)
+
+c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+console.log("Side C:", c);
+*/
+
+// document.getElementById("submitButton").onclick = function () {
+  
+// a = document.getElementById("aTextBox").value;
+// a = Number(a);
+
+// b = document.getElementById("bTextBox").value;
+// b = Number(b);
+
+// c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+  
+//   document.getElementById("cLabel").innerHTML = "Side C: " + c;
+
+// }
+
+
+
+
+                              //  Counter program #
+
+
+// let count = 0;
+
+// document.getElementById("decreaseBtn").onclick = function () {
+//   count-=1
+//   document.getElementById("countLabel").innerHTML = count;
+
+// }
+// document.getElementById("resetBtn").onclick = function () {
+//   count=0
+//   document.getElementById("countLabel").innerHTML = count;
+  
+// }
+// document.getElementById("increaseBtn").onclick = function () {
+//   count+=1
+//   document.getElementById("countLabel").innerHTML = count;
+  
+// }
+
+
+
+                              //  Random Number
+
+
+
+// let x;
+// let y;
+// let z;
+
+// document.getElementById("rollBtn").onclick = function (){
+  
+//   x = Math.floor(Math.random() * 6) + 1;
+//   y = Math.floor(Math.random() * 6) + 1;
+//   z = Math.floor(Math.random() * 6) + 1;
+
+//   document.getElementById("xlabel").innerHTML = x;
+//   document.getElementById("ylabel").innerHTML = y;
+//   document.getElementById("zlabel").innerHTML = z;
+// }
+
+
+
+
+                       //  Useful string propertoes & methods
+
+
+// let userName = "   Bro Zero   ";
+// let phoneNumber = "123-234-3455"
+
+// console.log(userName.length);
+// console.log(userName.charAt(1));
+// console.log(userName.indexOf("o"));
+// console.log(userName.lastIndexOf("o"));
+// userName = userName.trim();
+// userName = userName.toUpperCase();
+// userName = userName.toLowerCase();
+
+// phoneNumber = phoneNumber.replaceAll("-", "");
+
+// console.log(phoneNumber);
+
+
+
+
+                              //  String slicing
+
+
+
+
+
+
+
 
 
 
