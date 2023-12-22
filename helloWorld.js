c54fe2234572f761298bf8cc34647a832b7efcef
AsilbekNosirov <asilbeknosirov20202003@gmail.com>
2023-12-22 15:31:24 +0900

    JS007-Basic Number Guessing Game.

--- helloWorld.js
+++ helloWorld.js
@@ -685,4 +685,43 @@ console.log(myNum);
 */
 
 
+                      // Number Guessing Game
+
+
+const answer = Math.floor(Math.random() * 10 + 1);
+let guesses = 0;
+
+document.getElementById("submitButton").onclick = function () {
+
+  let guess = document.getElementById("guessField").value
+  guesses += 1;
+  
+  if (guess == answer) {
+    alert(`${answer} is the #. It took you ${guesses} guesses.`);
+  }
+  else if (guess < answer) {
+    alert("Too small!");
+  }
+  else {
+    alert("Too large!");
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
