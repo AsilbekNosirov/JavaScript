4b363e53934d6adbd850d54db611bca332e6b401
AsilbekNosirov <asilbeknosirov20202003@gmail.com>
2024-01-10 00:03:19 +0900

    JS011-Rest parameters

--- helloWorld.js
+++ helloWorld.js
@@ -839,6 +839,36 @@ console.log(...class1);
 
 
 
+                      // Rest Parameters
+
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+
+console.log(sum(a, b, c, d, e));
+
+function sum(x, y, ...numbers) {
+  let total = 0;
+  for (let number of numbers) {
+    total += numbers
+  }
+  return total
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
