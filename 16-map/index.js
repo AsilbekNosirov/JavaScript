b9a36e5560f61abfea10d8bc4e11c1c6f6d2ad82
AsilbekNosirov <asilbeknosirov20202003@gmail.com>
2024-01-24 18:28:01 +0900

    map method

--- 16-map/index.js
+++ 16-map/index.js
@@ -1,6 +1,11 @@
 // Map method
 
+const dates = ["2023-12-23", "2022-12-13", "2024-12-25"];
+const formattedDates = dates.map(formatDates);
 
+console.log(formattedDates);
 
-
-
+function formatDates(element) {
+    const parts = element.split("-");
+    return `${parts[1]}.${parts[2]}.${parts[1]}`;
+}
