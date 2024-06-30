961920492fab916bb6d950e218c0327a9c02b7cb
AsilbekNosirov <asilbeknosirov20202003@gmail.com>
2024-06-30 18:05:39 +0900

    setTimeout()

---
+++ 36-setTimeout/index.js
@@ -0,0 +1,18 @@
+
+// -------- setTimeouut() ---------
+
+let timeoutId;
+
+function startTimer(){
+    timeoutId = setTimeout(() => window.alert("Hello"), 3000);
+    console.log("STARTED");
+}
+
+function clearTimer(){
+    clearTimeout(timeoutId);
+    console.log("CLEARED");
+}
+
+
+
+
